d8195e4ece47694edb65ef4f42ce28a38fbf0991
Emin <emingench@gmail.com>
2020-10-01 17:30:33 +0300

    models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name= models.CharField(max_length=200)
+    phone= models.CharField(max_length=200)
+    email= models.EmailField( max_length=254)
+    date_created = models.DateTimeField(auto_now_add)
