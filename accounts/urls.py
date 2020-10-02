4767d8ed5aa3b5fa6839a14cdd9036fdd47f65b1
Emin <emingench@gmail.com>
2020-10-02 14:44:52 +0300

    crud

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('customer/', views.customer),
-    path('products/', views.products),
+    path('', views.home, name="home"),
+    path('customer/<str:pk>', views.customer,name="customer"),
+    path('products/', views.products,name="products"),
+    path('create_order', views.createOrder,name="createOrder"),
+    path('update_order/<str:pk>', views.updateOrder,name="updateOrder"),
+    path('delete_order/<str:pk>', views.deleteOrder,name="deleteOrder"),
 
 ]
