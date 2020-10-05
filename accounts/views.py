dd82e775c3a2f1f29f720f7bdf14400961b4ef50
Emin <emingench@gmail.com>
2020-10-05 17:34:34 +0300

    part 15

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.forms import inlineformset_factory
 from django.contrib import messages
 from django.contrib.auth import authenticate , login , logout 
 from django.contrib.auth.decorators import login_required
+from .decorators import *
 
 
 
@@ -13,43 +14,39 @@ from .filters import *
 
 # Create your views here.
 
+
+@unauthenticated_user
 def register(request):
-	if request.user.is_authenticated:
-		return redirect("home")
-	else:
-		form = CreateUserForm()
+	form = CreateUserForm()
 
-		if request.method == "POST":
-			form = CreateUserForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request,'Account was created for '+user)
-				return redirect("login")
+	if request.method == "POST":
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user = form.cleaned_data.get('username')
+			messages.success(request,'Account was created for '+user)
+			return redirect("login")
 
-		context={'form':form}
-		return render(request,'accounts/register.html',context) 
+	context={'form':form}
+	return render(request,'accounts/register.html',context) 
 
 
-
+@unauthenticated_user
 def loginPage(request):
-	if request.user.is_authenticated:
-		return redirect("home")
-	else:	
-		if request.method == "POST":
-			username=request.POST.get("username")
-			password=request.POST.get("password")
+	if request.method == "POST":
+		username=request.POST.get("username")
+		password=request.POST.get("password")
 
-			user = authenticate(request,username=username,password=password)
+		user = authenticate(request,username=username,password=password)
 
-			if user is not None:
-				login(request, user) 
-				return redirect("home")
-			else:
-				messages.info(request,"Username or Password is incorrect!")
+		if user is not None:
+			login(request, user) 
+			return redirect("home")
+		else:
+			messages.info(request,"Username or Password is incorrect!")
 
-		context={}
-		return render(request,'accounts/login.html',context)
+	context={}
+	return render(request,'accounts/login.html',context)
 
 
 
@@ -61,6 +58,7 @@ def logoutUser(request):
 
 
 @login_required(login_url="login")
+@allowed_users(allowed_roles=['admin','staff'])
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
