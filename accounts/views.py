5c70b078207dd2edd1438e7d940b51241b1e489d
Emin <emingench@gmail.com>
2020-10-06 14:50:22 +0300

    part 19

--- accounts/views.py
+++ accounts/views.py
@@ -25,11 +25,7 @@ def register(request):
 			user = form.save()
 			username = form.cleaned_data.get('username')
 			
-			group = Group.objects.get(name='customer')
-			user.groups.add(group)
-			Customer.objects.create(
-				user=user
-			)
+			
 
 			messages.success(request,'Account was created for '+username)
 			return redirect("login")
